42ea1973143fd6d28ed51945451a71ef6cc4c612
AndriiHliuza <gl093770@gmail.com>
2023-03-20 13:39:02 +0200

    Initial commit

---
+++ src/test/java/com/andrii/itea/simplenotes/SimplenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrii.itea.simplenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
